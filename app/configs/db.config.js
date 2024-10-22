905c76ea1f7e0775d8eff6342923b1f8d10e5721
connorrewa <connorrewa13@hotmail.com>
2024-10-21 21:37:12 -0400

    chore: edit railway connection string

--- app/configs/db.config.js
+++ app/configs/db.config.js
@@ -1,4 +1,10 @@
 const { Pool } = require('pg');
 require('dotenv').config();
-const pool = new Pool({ DATABASE_PUBLIC_URL });
+const pool = new Pool({
+    host: process.env.PGHOST,
+    user: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABASE,
+    port: process.env.PGPORT,
+});
 module.exports = pool;
